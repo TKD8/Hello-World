f267bd66e98ace08da37a527341eea35bc2b46fd
Natty <nmesfin2@go.shoreline.edu>
2018-03-06 02:41:15 -0800

    modified inode'

--- Inode.java
+++ Inode.java
@@ -1,6 +1,7 @@
 public class Inode {
    private final static int iNodeSize = 32;       // fix to 32 bytes
-   private final static int directSize = 11;      // # direct pointers
+   public final static int directSize = 11;      // # direct pointers
+   private final static int maxBytes = 512;         // maximum size
 
    public int length;                             // file size in bytes
    public short count;                            // # file-table entries pointing to this
@@ -27,25 +28,26 @@ public class Inode {
       // read from blockNumber to data
       SysLib.rawread(blockNumber, data);
       // starting location
-      int offest = (iNumber % 16) * iNodeSize;
-      length = SysLib.byts2int(data,offset);
+      int offset = (iNumber % 16) * iNodeSize;
+      length = SysLib.bytes2int(data,offset);
       offset += 4;
       count = SysLib.bytes2short(data,offset);
       offset += 2;
       flag = SysLib.bytes2short(data,offset);
-      offest += 2;
+      offset += 2;
 
       for(int i = 0; i < directSize; i++)
       {
         direct[i] = SysLib.bytes2short(data,offset);
-        offest += 2;
+        offset += 2;
       }
 
-      indirect = SysLib.bytes2short(data,offest);
+      indirect = SysLib.bytes2short(data,offset);
    }
 
    // save to disk as the i-th inode
    int toDisk( short iNumber ) {  
+        int offset = 0;
         // check for valid iNumber
         if(iNumber < 0)
         {
@@ -76,4 +78,82 @@ public class Inode {
         SysLib.rawwrite(blockNumber, data);
         return 0;
    } 
+   
+
+    byte[] freeIndirectBlock(){
+        if (indirect >= 0)
+        {
+            byte[] data = new byte[maxBytes];
+            SysLib.rawread(indirect, data);
+            indirect = -1;
+            return data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    int findTargetBlock(int offset){
+        int target = offset / maxBytes;
+        if (target < directSize){
+            return direct[target];
+        }
+        if (indirect < 0){
+            return -1;
+        }
+        byte[] data = new byte[maxBytes];
+        SysLib.rawread(indirect, data);
+        int blockSpace = (target - directSize) * 2;
+        return SysLib.bytes2short(data, blockSpace);
+    }
+
+    int getIndexBlockNumber(int entry, short offset){
+        int target = entry/maxBytes;
+        if (target < directSize){
+            if(direct[target] >= 0){
+                return -1;
+            }
+            if ((target > 0 ) && (direct[target - 1 ] == -1)){
+                return -2;
+            }
+            direct[target] = offset;
+            return 0;
+        }
+        if (indirect < 0){
+            return -3;
+        }
+        else{
+            byte[] data = new byte[maxBytes];
+            SysLib.rawread(indirect,data);
+            int blockSpace = (target - directSize) * 2;
+            if ( SysLib.bytes2short(data, blockSpace) > 0){
+                return -1;
+            }
+            else
+            {
+                SysLib.short2bytes(offset, data, blockSpace);
+                SysLib.rawwrite(indirect, data);
+            }
+        }
+        return 0;
+    }
+
+    boolean setIndexBlock(short blockNumber){
+        for (int i = 0; i < directSize; i++){
+            if (direct[i] == -1){
+                return false;
+            }
+        }
+        if (indirect != -1){
+            return false;
+        }
+        indirect = blockNumber;
+        byte[ ] data = new byte[maxBytes];
+        for(int i = 0; i < (maxBytes/2); i++){
+            SysLib.short2bytes((short) -1, data, i * 2);
+        }
+        SysLib.rawwrite(blockNumber, data);
+        return true;
+    }
 }
